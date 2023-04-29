3c64bb1fa13034833d005e1df5be27eb0d1b691c
natthaphon-wr <natthaphon.win@gmail.com>
2023-04-29 17:00:12 +0700

    Update fbBoots_notebook.R

--- Project/fbBoots_notebook.R
+++ Project/fbBoots_notebook.R
@@ -7,6 +7,9 @@ library(readr)
 library(ggplot2)
 library(cluster)
 library(fpc)
+library(reshape2)
+library(purrr)
+library(dendextend)
 
 # Data Preparation -------------------------------------------------------------
 fbBootDB <- read_csv('footballbootsdb.csv')
@@ -127,7 +130,7 @@ plot(hc_average, main = "Agglomerative, average linkages")
 # Complete and average are quite balanced in the same level
 
 ## No. of cluster Measurement -------------------------------
-# I think the suitable no. of cluster are 4-7. 
+# I think the suitable no. of cluster are 4-10. 
 # (more than 3 because there are 3 large brands)
 
 # This function from https://towardsdatascience.com/hierarchical-clustering-on-categorical-data-in-r-a27e578f2995 
@@ -166,20 +169,123 @@ cstats.table <- function(dist, tree, k) {
   return(output)
 }
 
-stats_hc_c <- cstats.table(gower_dist, hc_complete, 7)
-stats_hc_c
-stats_hc_avg <- cstats.table(gower_dist, hc_average, 7)
-stats_hc_avg
-# See that complete linkage is more balance
-
-### Elbow ------------------------------------------------
+### Within clusters SS --------------------------------------------
 ggplot(data = data.frame(t(cstats.table(gower_dist, hc_complete, 15))), 
        aes(x=cluster.number, y=within.cluster.ss)) + 
   geom_point()+
   geom_line()+
-  ggtitle("Agglomerative clustering") +
+  ggtitle("Clustering (Complete Linkage)") +
   labs(x = "Num.of clusters", y = "Within clusters sum of squares (SS)") +
   theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(data = data.frame(t(cstats.table(gower_dist, hc_average, 15))), 
+       aes(x=cluster.number, y=within.cluster.ss)) + 
+  geom_point()+
+  geom_line()+
+  ggtitle("Clustering (Average Linkage)") +
+  labs(x = "Num.of clusters", y = "Within clusters sum of squares (SS)") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+### Silhouette Width --------------------------------------------
+ggplot(data = data.frame(t(cstats.table(gower_dist, hc_complete, 15))), 
+       aes(x=cluster.number, y=avg.silwidth)) + 
+  geom_point()+
+  geom_line()+
+  ggtitle("Clustering (Complete Linkage)") +
+  labs(x = "Num.of clusters", y = "Average silhouette width") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(data = data.frame(t(cstats.table(gower_dist, hc_average, 15))), 
+       aes(x=cluster.number, y=avg.silwidth)) + 
+  geom_point()+
+  geom_line()+
+  ggtitle("Clustering (Average Linkage)") +
+  labs(x = "Num.of clusters", y = "Average silhouette width") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+### Selection --------------------------------------------------
+# From complete linkage graphs, the optimal number is 6 or 12.
+# From average linkage graphs, the optimal number is 4 or 14.
+# However, I think 4 groups are quite small, and 12/14 are too much.
+# So, I will use complete linkage with no. of group is 6.
+
+stats_hc_c <- cstats.table(gower_dist, hc_complete, 10)
+stats_hc_c
+# From this statistic in Test5 (n=6), the balance is acceptable.
+
+
+## Visualize Result --------------------------------------------------------
+cut_best <- cutree(hc_complete, 6)
+# plot(hc_complete, main="Complete Linkage", check=TRUE)
+# rect.hclust(hc_complete, k=6, border = 2:6)
+# abline(h=3, col = 'red')
+
+### Dendrogram ----------------------------------------
+dendro_comp <- as.dendrogram(hc_complete)
+comp_col_dend <- color_branches(dendro_comp, k=6)
+plot(comp_col_dend, main="Dendrogram, k = 6")
+
+Boots_cluster <- mutate(prep_data, cluster=cut_best)
+count(Boots_cluster, cluster)
+Boots_cluster$cluster <- as.factor(Boots_cluster$cluster)
+summary(Boots_cluster)
+
+### Dendrogram2 ----------------------------------------
+# dendro <- as.dendrogram(hc_complete)
+# dendro.col <- dendro %>%
+#   set("branches_k_color", k=6) %>%
+#   set("branches_lwd", 0.6) %>%
+#   set("labels_colors", value = c("darkslategray")) %>%
+#   set("labels_cex", 0.5)
+# ggd1 <- as.ggdend(dendro.col)
+# ggplot(ggd1, theme=theme_minimal()) +
+#   labs(x="Number of Observations", y="Height", title="Dendrogram, k = 6")
+
+### Radical Plot -----------------------------------------
+# ggplot(ggd1, labels=T) + 
+#   scale_y_reverse(expand = c(0.2, 0)) +
+#   coord_polar(theta="x")
+
+### Heatmap -----------------------------------------------
+clust.num <- cutree(hc_complete, k=6)
+prepData.cl <- cbind(prep_data, clust.num)
+cust.long <- melt(data.frame(lapply(prepData.cl, as.character), stringsAsFactors=FALSE),
+                  id = c("RecordID", "clust.num"), factorsAsStrings=T)
+cust.long.q <- cust.long %>%
+  group_by(clust.num, variable, value) %>%
+  mutate(count = n_distinct(RecordID)) %>%
+  distinct(clust.num, variable, value, count)
+cust.long.p <- cust.long.q %>%
+  group_by(clust.num, variable) %>%
+  mutate(perc = count / sum(count)) %>%
+  arrange(clust.num)
+heatmap.p <- ggplot(cust.long.p, aes(x = clust.num, y = factor(value, ordered = T))) +
+  geom_tile(aes(fill = perc), alpha = 0.85) +
+  labs(title = "Distribution of characteristics across clusters", x = "Cluster number", y = NULL) +
+  geom_hline(yintercept = 3.5) +
+  geom_hline(yintercept = 10.5) +
+  geom_hline(yintercept = 13.5) +
+  geom_hline(yintercept = 17.5) +
+  geom_hline(yintercept = 21.5) +
+  scale_fill_gradient2(low = "darkslategray1", mid = "yellow", high = "turquoise4")
+heatmap.p
+
+
+### Plot each variable --------------------------------
+# Explore important variables.
+
+ggplot(Boots_cluster, aes(x=1:dim(Boots_cluster)[1], y=PlayerMarketValue, color=cluster)) + 
+  geom_point() +
+  xlab("Data") +
+  ggtitle("Boots Data with Clustering")
+
+barplot(table(Boots_cluster$BootsPosition, Boots_cluster$cluster),
+        beside=TRUE,
+        legend.text = TRUE,
+        main="BootsPosition with Clustering",
+        xlab="Cluster",
+        ylab="Count")
 
 
 
